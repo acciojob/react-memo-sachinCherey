381eb24d0c34888917c285e93b7196655f1166e9
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-09-16 00:54:48 +0000

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,71 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react';
 
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todoInputValue, setTodoInputValue] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [memoInputValue, setMemoInputValue] = useState('');
+  const [memos, setMemos] = useState([]);
+
+  const handleAddTodo = () => {
+    if (todoInputValue.length >= 5) {
+      setTodos([...todos, todoInputValue]);
+      setTodoInputValue('');
+    } else {
+      alert('Todo must be at least 5 characters long.');
+    }
+  };
+
+  const handleAddMemo = () => {
+    if (memoInputValue.length >= 5) {
+      setMemos([...memos, memoInputValue]);
+      setMemoInputValue('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>React.useMemo</h1>
+      <h1>My Todos</h1>
+      <p>New Todo</p>
+      <div id="todo">
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+      <input
+        id="mD"
+        type="text"
+        minLength={5}
+        value={todoInputValue}
+        onChange={(e) => setTodoInputValue(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <hr />
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <br />
+      <h1>Expensive Calculation</h1>
+      <h1>React.memo</h1>
+      <input
+        id="m"
+        type="text"
+        minLength={5}
+        value={memoInputValue}
+        onChange={(e) => setMemoInputValue(e.target.value)}
+      />
+      <button onClick={handleAddMemo}>Add Skill</button>
+      <div id="memo">
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={index}>{memo}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
